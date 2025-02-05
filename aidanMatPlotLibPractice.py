95c2860930d2a2540456a7a7154febbc7abf9ef0
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2025-02-05 11:55:16 -0600

    got aidanMatPlotLibPractice.py to work

--- aidanMatPlotLibPractice.py
+++ aidanMatPlotLibPractice.py
@@ -1,9 +1,32 @@
 import matplotlib.pyplot as plot
 import numpy as np
+import csv
 
-x = np.linspace(0,10,100)
+result = {
+    "Date" and 0:[],
+    "Open" and 1:[],
+    "High" and 2:[],
+    "Low" and 3:[],
+    "Close" and 4:[],
+    "Adj Close" and 5:[],
+    "Volume" and 6:[]
+}
 
+
+with open("microsoft_stock_price.csv", newline="") as csvfile:
+    read = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for n,row in enumerate(read):
+        if n == 0: continue
+        else:
+            resultArray = row[0].split(",")
+            for i,v in enumerate(resultArray):
+                #print(i,v)
+                result[i].append(v)
+a = []
+b = []
+for count,v in enumerate(result[1]):
+    a.append(count)
+    b.append(float(v))
 fig, ax = plot.subplots()
-ax.grid(visible=True)
-ax.plot(x,x**2)
+ax.plot(a,b)
 plot.show()
