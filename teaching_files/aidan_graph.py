81e3a0b1e8f5a4b18cedebd1454896c2bd851c0c
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2025-02-06 11:38:19 -0600

    Made aidan_graph look fancier

--- teaching_files/aidan_graph.py
+++ teaching_files/aidan_graph.py
@@ -24,7 +24,16 @@ with open("teaching_files/microsoft_stock_price.csv", newline="") as csvfile:
                 #print(i,v)
                 result[i].append(v)
 fig, ax = plot.subplots()
-a = [dt.datetime.strptime(d, "%Y-%m-%d").date() for d in result[0]]
-b = [float(v) for v in result[1]]
-ax.plot(a,b)
+a = [dt.datetime.strptime(d, "%Y-%m-%d").date() for d in result[0]] # thanks to https://stackoverflow.com/questions/9627686/plotting-dates-on-the-x-axis for helping with datetime math
+b = [float(v) for v in result[2]]
+c = [float(v) for v in result[3]]
+d = [float(v) for v in result[5]]
+avg = [(float(result[2][i]) + float(result[3][i]))/2 for i in range(0, len(result[2]))]
+
+ax.plot(a,avg, label = "Average Price", color = "green", linewidth = 1)
+ax.plot(a,d, label = "Trade Volume", color = "blue", linestyle = "--", linewidth = 1)
+plot.title("Microsoft Stock Price Over Time")
+plot.xlabel("Date")
+plot.ylabel("Price (USD)")
+plot.legend()
 plot.show()
