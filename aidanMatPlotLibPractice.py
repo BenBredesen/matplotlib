b8e0f3ac7864c16b1155e950968b3b7c6637ebd8
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2025-02-06 11:08:19 -0600

    Added datetime math to my script

--- aidanMatPlotLibPractice.py
+++ aidanMatPlotLibPractice.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plot
 import numpy as np
 import csv
+import datetime as dt
 
 result = {
     "Date" and 0:[],
@@ -13,6 +14,7 @@ result = {
 }
 
 
+
 with open("microsoft_stock_price.csv", newline="") as csvfile:
     read = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for n,row in enumerate(read):
@@ -22,11 +24,10 @@ with open("microsoft_stock_price.csv", newline="") as csvfile:
             for i,v in enumerate(resultArray):
                 #print(i,v)
                 result[i].append(v)
-a = []
-b = []
-for count,v in enumerate(result[1]):
-    a.append(count)
-    b.append(float(v))
+a = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in result[0]] #datetime math added in with the help of https://stackoverflow.com/questions/9627686/plotting-dates-on-the-x-axis
+b = [float(v) for v in result[1]] #opening price
+c = [float(v) for v in result[4]] #closing price
 fig, ax = plot.subplots()
 ax.plot(a,b)
+ax.plot(a,c)
 plot.show()
