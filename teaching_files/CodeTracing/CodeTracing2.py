eb3adf63a6983d434bb40254d74c949f22111539
RevenantRobbie <143741198+RevenantRobbie@users.noreply.github.com>
2025-02-08 16:35:36 -0600

    added some code tracing problems

---
+++ teaching_files/CodeTracing/CodeTracing2.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plot
+import numpy as np
+
+fig, ax = plot.subplots()
+
+result = [[],[]]
+for n in range(-700,700,10):
+    result[0].append(np.sin(n/100)*0.5)
+    result[1].append(n/100)
+
+ax.plot(result[0],result[1], lw = 0.5)
+plot.show()
---
+++ teaching_files/CodeTracing/CodeTracing3.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plot
+
+SurveyResults = [
+    ["Yes", "No", "Maybe", "invisible"],
+    [100,22, 68, -18],
+    ["red", "orange", "yellow", "white"]]
+
+fig, ax = plot.subplots(figsize=(5,2.7), layout='constrained')
+ax.bar(SurveyResults[0],SurveyResults[1],color = SurveyResults[2])
+plot.show()
